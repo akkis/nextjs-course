b14577636573d272f84c471ccd1871e89744c4dd
Akis Laoutaris <alaoutaris@nelios.com>
2022-08-18 12:35:48 +0300

    course 65

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -5,9 +5,16 @@ function ClientProjectsPage() {
 
   console.log(router.query);
 
+  function loadProjectHandler() {
+    // do work with remote data or something...
+
+    router.push('/clients/max/projecta');
+  }
+
   return (
     <div>
       <h1>Projects of a given client page</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
