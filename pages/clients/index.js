169f8fe379e454a8d1abb25debf2a2812ab808bf
Akis Laoutaris <alaoutaris@nelios.com>
2022-08-18 12:09:24 +0300

    course continue

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,26 @@
+import Link from "next/link";
+
 function ClientsPage() {
+  const clients = [
+    {
+      id: "max",
+      name: "Maximilian",
+    },
+    {
+      id: "manuel",
+      name: "Manuel",
+    },
+  ];
   return (
     <div>
       <h1>Clients page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
